b82fac400c6e7d6bc51277dc745e71d863a15cc8
Benjamin Casalino <benjamincasalino@Benjamins-MacBook-Pro.local>
2015-09-18 14:32:12 -0700

    complete specs js and add bootstrap/ formatting to front end

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+function Topping(toppingType) {
+    this.toppingType = toppingType;
+}
+
+// Pizza
+function Pizza(pizzaSize, pizzaType, toppings, pizzaCost) {
+    "use strict";
+    this.pizzaSize = pizzaSize;
+    this.pizzaType = pizzaType;
+    this.toppings = toppings;
+    this.pizzaCost = pizzaCost;
+}
+////////////////////////pizza prototype /////////////////////////////////////////
+Pizza.prototype.calculatePizzaCost = function() {
+    var cost = 0;
+    if (this.pizzaSize === "small") {
+        cost += 1;
+    } else if (this.pizzaSize === "medium") {
+        cost += 2;
+    } else if (this.pizzaSize === "large") {
+        cost += 3;
+    }
+    //calclulate cost for each topping at this spot
+    for (var topping in this.toppings) {
+        cost += 1;
+    }
+    this.pizzaCost = cost;
+};
+
+// Order
+function Order(cartItems, orderTotal) {
+    "use strict";
+    this.cartItems = cartItems;
+    this.orderTotal = orderTotal;
+}
+////////////////////////////// order prototype /////////////////////////////////
+Order.prototype.calculateTotal = function() {
+    // order starts at 0
+    var pizzaTotal = 0;
+    for (var pizzaIndex in this.cartItems) {
+        pizzaTotal += this.cartItems[pizzaIndex].pizzaCost;
+    }
+    this.orderTotal = pizzaTotal;
+};
